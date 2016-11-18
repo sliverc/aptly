5d5fcae97e5a51b2df0b4465bef223367be2e294
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2016-11-18 15:46:49 +0100

    every go routine needs to have its own collection factory
    this is needed so concurrent reads and writes are possible.

--- cmd/repo_include.go
+++ cmd/repo_include.go
@@ -36,6 +36,8 @@ func aptlyRepoInclude(cmd *commander.Command, args []string) error {
 	ignoreSignatures := context.Flags().Lookup("ignore-signatures").Value.Get().(bool)
 	noRemoveFiles := context.Flags().Lookup("no-remove-files").Value.Get().(bool)
 
+	collectionFactory := context.NewCollectionFactory()
+
 	repoTemplate, err := template.New("repo").Parse(context.Flags().Lookup("repo").Value.Get().(string))
 	if err != nil {
 		return fmt.Errorf("error parsing -repo template: %s", err)
@@ -98,7 +100,7 @@ func aptlyRepoInclude(cmd *commander.Command, args []string) error {
 		context.Progress().Printf("Loading repository %s for changes file %s...\n", repoName.String(), changes.ChangesName)
 
 		var repo *deb.LocalRepo
-		repo, err = context.CollectionFactory().LocalRepoCollection().ByName(repoName.String())
+		repo, err = collectionFactory.LocalRepoCollection().ByName(repoName.String())
 		if err != nil {
 			failedFiles = append(failedFiles, path)
 			reporter.Warning("unable to process file %s: %s", changes.ChangesName, err)
@@ -127,13 +129,13 @@ func aptlyRepoInclude(cmd *commander.Command, args []string) error {
 			}
 		}
 
-		err = context.CollectionFactory().LocalRepoCollection().LoadComplete(repo)
+		err = collectionFactory.LocalRepoCollection().LoadComplete(repo)
 		if err != nil {
 			return fmt.Errorf("unable to load repo: %s", err)
 		}
 
 		var list *deb.PackageList
-		list, err = deb.NewPackageListFromRefList(repo.RefList(), context.CollectionFactory().PackageCollection(), context.Progress())
+		list, err = deb.NewPackageListFromRefList(repo.RefList(), collectionFactory.PackageCollection(), context.Progress())
 		if err != nil {
 			return fmt.Errorf("unable to load packages: %s", err)
 		}
@@ -153,7 +155,7 @@ func aptlyRepoInclude(cmd *commander.Command, args []string) error {
 		var processedFiles2, failedFiles2 []string
 
 		processedFiles2, failedFiles2, err = deb.ImportPackageFiles(list, packageFiles, forceReplace, verifier, context.PackagePool(),
-			context.CollectionFactory().PackageCollection(), reporter, restriction, context.CollectionFactory().ChecksumCollection())
+			collectionFactory.PackageCollection(), reporter, restriction, collectionFactory.ChecksumCollection())
 
 		if err != nil {
 			return fmt.Errorf("unable to import package files: %s", err)
@@ -161,7 +163,7 @@ func aptlyRepoInclude(cmd *commander.Command, args []string) error {
 
 		repo.UpdateRefList(deb.NewPackageRefListFromPackageList(list))
 
-		err = context.CollectionFactory().LocalRepoCollection().Update(repo)
+		err = collectionFactory.LocalRepoCollection().Update(repo)
 		if err != nil {
 			return fmt.Errorf("unable to save: %s", err)
 		}
